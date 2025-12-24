b29da09c18d6dc85b88db61349195ee099627d02
sabaris-sps <spssabaris@gmail.com>
2025-12-24 21:15:28 +0530

    updated with dft 2

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -3,6 +3,59 @@ import { TestEntry } from '../types';
 
 export const MOCK_ENTRIES: TestEntry[] = [
   {
+    "id": "53959b24-88e3-4feb-a3e1-cf43d9212fd6",
+    "date": "2025-12-24",
+    "testName": "DFT 2",
+    "physics": {
+      "marks": 90,
+      "unattempted": 0,
+      "incorrect": 2,
+      "accuracy": 92,
+      "calcError": 1,
+      "misconception": 0,
+      "conceptNotAware": 1,
+      "readingError": 0,
+      "extraThinking": 0,
+      "lackOfTime": 0
+    },
+    "chemistry": {
+      "marks": 80,
+      "unattempted": 0,
+      "incorrect": 4,
+      "accuracy": 84,
+      "calcError": 1,
+      "misconception": 0,
+      "conceptNotAware": 3,
+      "readingError": 0,
+      "extraThinking": 0,
+      "lackOfTime": 0
+    },
+    "maths": {
+      "marks": 91,
+      "unattempted": 1,
+      "incorrect": 1,
+      "accuracy": 95.83,
+      "calcError": 1,
+      "misconception": 0,
+      "conceptNotAware": 0,
+      "readingError": 0,
+      "extraThinking": 0,
+      "lackOfTime": 0
+    },
+    "total": {
+      "marks": 261,
+      "unattempted": 1,
+      "incorrect": 7,
+      "accuracy": 90.54,
+      "calcError": 3,
+      "misconception": 0,
+      "conceptNotAware": 4,
+      "readingError": 0,
+      "extraThinking": 0,
+      "lackOfTime": 0
+    }
+  },
+  {
     "id": "a73e4b4e-0876-49a8-8ef5-8f5eb35b86f0",
     "date": "2025-12-22",
     "testName": "DFT 1",
